ab79e73fe7d990bdcfa024c999420407929d2a78
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-06-09 17:31:00 -0300

    Criação dos arquivos: Cliente.cs, ClienteController.cs & Index.cshtml.

---
+++ 1 - Basic/Aula1AspNetMVC/Aula1AspNetMVC/Controllers/ClienteController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Mvc;
+using Aula1AspNetMVC.Models;
+
+namespace Aula1AspNetMVC.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            var cliente = new Cliente()
+            {
+                Id = 1,
+                Nome = "Glaucia",
+                Sobrenome = "Lemos",
+                DataCadastro = DateTime.Now
+            };
+
+            return View(cliente);
+        }
+    }
+}
---
+++ 1 - Basic/Aula1AspNetMVC/Aula1AspNetMVC/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aula1AspNetMVC.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+    }
+}
