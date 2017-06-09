6fa7a1a3eca8c6c39a95852e6bbabeca31e9dad7
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-06-09 18:38:18 -0300

    Alteração na nomenclatura do código de pt-br para en

---
+++ 1 - Basic/Aula1AspNetMVC/Aula1AspNetMVC/Controllers/ClientController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Aula1AspNetMVC.Models;
+
+namespace Aula1AspNetMVC.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Test()
+        {
+            var client = new Client()
+            {
+                Id = 1,
+                Name = "Glaucia",
+                Surname = "Lemos",
+                CreatedDate = DateTime.Now
+            };
+
+            return View("Index", client);
+        }
+
+        public ActionResult ListClient()
+        {
+            var clientList = new List<Client>()
+            {
+                new Client(){ Id = 1, Name = "José", Surname = "Lemos", CreatedDate = DateTime.Now },
+                new Client(){ Id = 2, Name = "Jurema", Surname = "Lemos", CreatedDate = DateTime.Now },               
+            };
+
+            return View(clientList);
+        }
+
+        public ActionResult SearchClient(string id)
+        {
+            var clientList = new List<Client>()
+            {
+                new Client(){ Id = 1, Name = "José", Surname = "Lemos", CreatedDate = DateTime.Now },
+                new Client(){ Id = 2, Name = "Jurema", Surname = "Lemos", CreatedDate = DateTime.Now },
+                new Client(){ Id = 3, Name = "Jake", Surname = "Lemos", CreatedDate = DateTime.Now },
+                new Client(){ Id = 4, Name = "Edízio", Surname = "Lemos", CreatedDate = DateTime.Now }
+            };
+
+            /* Aqui estamos realizando uma busca de clientes pelo Nome através do 'Id' */
+            var cliente = clientList.Where(c => c.Name == id).ToList();
+
+            return View("List", clientList);
+        }
+    }
+}
---
+++ 1 - Basic/Aula1AspNetMVC/Aula1AspNetMVC/Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aula1AspNetMVC.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
