34a4291bb47b95af1aa3134599b9c0890b190901
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-06-09 18:16:46 -0300

    Criando o método 'List' & 'Search' no Controller

--- 1 - Basic/Aula1AspNetMVC/Aula1AspNetMVC/Controllers/ClienteController.cs
+++ 1 - Basic/Aula1AspNetMVC/Aula1AspNetMVC/Controllers/ClienteController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using Aula1AspNetMVC.Models;
 
@@ -7,7 +9,7 @@ namespace Aula1AspNetMVC.Controllers
     public class ClienteController : Controller
     {
         // GET: Cliente
-        public ActionResult Index()
+        public ActionResult Teste()
         {
             var cliente = new Cliente()
             {
@@ -17,7 +19,34 @@ namespace Aula1AspNetMVC.Controllers
                 DataCadastro = DateTime.Now
             };
 
-            return View(cliente);
+            return View("Index", cliente);
+        }
+
+        public ActionResult Lista()
+        {
+            var listaClientes = new List<Cliente>()
+            {
+                new Cliente(){ Id = 2, Nome = "José", Sobrenome = "Lemos", DataCadastro = DateTime.Now },
+                new Cliente(){ Id = 3, Nome = "Jurema", Sobrenome = "Lemos", DataCadastro = DateTime.Now }
+            };
+
+            return View(listaClientes);
+        }
+
+        public ActionResult Pesquisa(string id)
+        {
+            var listaClientes = new List<Cliente>()
+            {
+                new Cliente(){ Id = 1, Nome = "José", Sobrenome = "Lemos", DataCadastro = DateTime.Now },
+                new Cliente(){ Id = 2, Nome = "Jurema", Sobrenome = "Lemos", DataCadastro = DateTime.Now },
+                new Cliente(){ Id = 3, Nome = "Jake", Sobrenome = "Lemos", DataCadastro = DateTime.Now },
+                new Cliente(){ Id = 4, Nome = "Edízio", Sobrenome = "Lemos", DataCadastro = DateTime.Now }
+            };
+
+            /* Aqui estamos realizando uma busca de clientes pelo Nome através do 'Id' */
+            var cliente = listaClientes.Where(c => c.Nome == id).ToList();
+
+            return View("Lista", cliente);
         }
     }
 }
