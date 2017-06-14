2b964788130a2b6d8fb2c077d77e6cd82e2f8e74
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-06-14 15:09:52 -0300

    Criação da classe construtor para fins de criar o contexto para EF.

--- 1 - Basic/Aula1AspNetMVC/Aula1AspNetMVC/Context/Aula1Context.cs
+++ 1 - Basic/Aula1AspNetMVC/Aula1AspNetMVC/Context/Aula1Context.cs
@@ -4,6 +4,10 @@ namespace Aula1AspNetMVC.Context
 {
     public class Aula1Context : DbContext
     {
-
+        public Aula1Context()
+            : base("Aula1Context")
+        {
+            
+        }
     }
 }
