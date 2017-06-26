1a2c1ef6646413da831f5b81c7853228e21489bb
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-06-25 23:16:18 -0300

    Criando summaries para fins de documentação das Entidades criadas.

--- 2 - Intermediary/AppEnterprise/src/GL.AppEnterprise.Domain/Entities/Address.cs
+++ 2 - Intermediary/AppEnterprise/src/GL.AppEnterprise.Domain/Entities/Address.cs
@@ -4,18 +4,39 @@ namespace GL.AppEnterprise.Domain.Entities
 {
     public class Address
     {
+        /// <summary>
+        /// Id do Endereço
+        /// </summary>
         public Guid AddressId { get; set; }
 
+        /// <summary>
+        /// Logradouro
+        /// </summary>
         public string PublicPlace { get; set; }
 
+        /// <summary>
+        /// Número do Endereço
+        /// </summary>
         public string Number { get; set; }
 
+        /// <summary>
+        /// Complemento 
+        /// </summary>
         public string AddressComplement { get; set; }
 
+        /// <summary>
+        /// CEP
+        /// </summary>
         public string Cep { get; set; }
 
+        /// <summary>
+        /// Cidade
+        /// </summary>
         public string City { get; set; }
 
+        /// <summary>
+        /// Estado
+        /// </summary>
         public string State { get; set; }
     }
 }
--- 2 - Intermediary/AppEnterprise/src/GL.AppEnterprise.Domain/Entities/Client.cs
+++ 2 - Intermediary/AppEnterprise/src/GL.AppEnterprise.Domain/Entities/Client.cs
@@ -4,18 +4,39 @@ namespace GL.AppEnterprise.Domain.Entities
 {
     public class Client
     {
+        /// <summary>
+        /// Id do Cliente
+        /// </summary>
         public Guid ClientId { get; set; }
 
+        /// <summary>
+        /// Nome do Cliente
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Email do Cliente
+        /// </summary>
         public string Email { get; set; }
 
+        /// <summary>
+        /// CPF do Cliente
+        /// </summary>
         public string Cpf { get; set; }
 
+        /// <summary>
+        /// Data de Criação do Cadastro do Cliente
+        /// </summary>
         public DateTime CreatedDate { get; set; }
 
+        /// <summary>
+        /// Data de Nascimento do Cliente
+        /// </summary>
         public DateTime BirthDate { get; set; }
 
+        /// <summary>
+        /// Saber se o Cliente está ativo ou não - Ativo
+        /// </summary>
         public bool Active { get; set; }
     }
 }
