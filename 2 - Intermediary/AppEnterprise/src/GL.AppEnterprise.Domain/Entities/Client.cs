4d2a3c0863dec2610a16814a03ae0068c3bb532c
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-06-25 23:04:58 -0300

    Criação da Entidade - Client.cs

---
+++ 2 - Intermediary/AppEnterprise/src/GL.AppEnterprise.Domain/Entities/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GL.AppEnterprise.Domain.Entities
+{
+    public class Client
+    {
+        public Guid ClientId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string CPF { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
