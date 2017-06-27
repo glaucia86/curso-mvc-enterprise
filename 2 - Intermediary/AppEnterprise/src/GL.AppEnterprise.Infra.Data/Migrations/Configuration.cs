ba1f8b1906f0f89d2862f63ee224d688330df1b0
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-06-26 21:38:11 -0300

    Execução do comando 'Enable-Migrations' na camada 'Data'.

---
+++ 2 - Intermediary/AppEnterprise/src/GL.AppEnterprise.Infra.Data/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace GL.AppEnterprise.Infra.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<GL.AppEnterprise.Infra.Data.Context.AppEnterpriseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(GL.AppEnterprise.Infra.Data.Context.AppEnterpriseContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
