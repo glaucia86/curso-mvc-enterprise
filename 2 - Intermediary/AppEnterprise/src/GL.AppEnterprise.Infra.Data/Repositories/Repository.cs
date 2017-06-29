e4cb054d8e612507fa399e5825c5325d777d98d9
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-06-29 19:50:10 -0300

    Criação do arquivo Repository.cs na camada 'Data'

---
+++ 2 - Intermediary/AppEnterprise/src/GL.AppEnterprise.Infra.Data/Repositories/Repository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using GL.AppEnterprise.Domain.Interfaces.Repository;
+
+namespace GL.AppEnterprise.Infra.Data.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public TEntity Add(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Search(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Update(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
