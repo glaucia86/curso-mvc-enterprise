24e94a4b1e918c777d9d600d7e59efcd1d20ed10
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-06-29 23:23:47 -0300

    Criação de um arquivo Resource.

--- 2 - Intermediary/AppEnterprise/src/GL.AppEnterprise.Application/ViewModel/ClientViewModel.cs
+++ 2 - Intermediary/AppEnterprise/src/GL.AppEnterprise.Application/ViewModel/ClientViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using GL.AppEnterprise.Domain.Properties;
 
 namespace GL.AppEnterprise.Application.ViewModel
 {
@@ -18,28 +19,32 @@ namespace GL.AppEnterprise.Application.ViewModel
         [Required(ErrorMessage = "Fill the field: Name")]
         [MaxLength(150, ErrorMessage = "Max {0} characters")]
         [MinLength(2, ErrorMessage = "Min {0} characters")]
+        [Display(ResourceType = typeof(Resources), Name = "Client_Name")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Fill the field: E-mail")]
         [MaxLength(100, ErrorMessage = "Max {0} characters")]
         [EmailAddress(ErrorMessage = "Invalid E-mail")]
-        [DisplayName("E-mail")]
+        [Display(ResourceType = typeof(Resources), Name = "Client_Email")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Fill the field: CPF")]
         [MaxLength(11, ErrorMessage = "Max {0} characters")]
         [DisplayName("CPF")]
+        [Display(ResourceType = typeof(Resources), Name = "Client_CPF")]
         public string Cpf { get; set; }
 
-        [Display(Name = "Birth of Date")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
         [DataType(DataType.Date, ErrorMessage = "Invalid Date Format")]
+        [Display(ResourceType = typeof(Resources), Name = "Client_BirthDate")]
         public DateTime BirthDate { get; set; }
 
         [ScaffoldColumn(false)]
+        [Display(ResourceType = typeof(Resources), Name = "Client_CreatedDate")]
         public DateTime CreatedDate { get; set; }
 
         [ScaffoldColumn(false)]
+        [Display(ResourceType = typeof(Resources), Name = "Client_Active")]
         public bool Active { get; set; }
     }
 }
