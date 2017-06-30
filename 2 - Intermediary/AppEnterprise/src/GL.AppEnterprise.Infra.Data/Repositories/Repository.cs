13ed5e2397e588f6fb31a1807aacc22539a8c5b8
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-06-29 21:15:48 -0300

    Implementação dos métodos no arquivo Repository.cs na camada 'Data'

--- 2 - Intermediary/AppEnterprise/src/GL.AppEnterprise.Infra.Data/Repositories/Repository.cs
+++ 2 - Intermediary/AppEnterprise/src/GL.AppEnterprise.Infra.Data/Repositories/Repository.cs
@@ -1,50 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 using System.Linq.Expressions;
 using GL.AppEnterprise.Domain.Interfaces.Repository;
+using GL.AppEnterprise.Infra.Data.Context;
 
 namespace GL.AppEnterprise.Infra.Data.Repositories
 {
     public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
+        protected AppEnterpriseContext Db;
+
+        protected DbSet<TEntity> DbSet;
+
+        public Repository()
+        {
+            Db = new AppEnterpriseContext();
+
+            DbSet = Db.Set<TEntity>(); //aqui estamos criando um atalho para deixar o código mais clean na hora da implementação
+        }
+
         public TEntity Add(TEntity obj)
         {
-            throw new NotImplementedException();
+            var returnObj = DbSet.Add(obj);
+            SaveChanges();
+
+            return returnObj;
         }
 
         public void Delete(Guid id)
         {
-            throw new NotImplementedException();
+            //Primeiro fazemos uma busca pelo id e depois removeremos.
+            DbSet.Remove(DbSet.Find(id));
+            SaveChanges();
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            Db.Dispose();
+            GC.SuppressFinalize(this); //GC = Garbage Collector irá remover o lixo da app
         }
 
-        public IEnumerable<TEntity> GetAll()
+        public IEnumerable<TEntity> GetAll() //int t, int s (parâmetros para trabalhar com paginação em memória)
         {
-            throw new NotImplementedException();
+            //return DbSet.Take(t).Skip(s).ToList(); //aqui um exemplo de paginação em memória
+            return DbSet.ToList();
         }
 
         public TEntity GetById(Guid id)
         {
-            throw new NotImplementedException();
+            return DbSet.Find(id);
         }
 
         public int SaveChanges()
         {
-            throw new NotImplementedException();
+            return Db.SaveChanges();
         }
 
         public IEnumerable<TEntity> Search(Expression<Func<TEntity, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return DbSet.Where(predicate);
         }
 
         public TEntity Update(TEntity obj)
         {
-            throw new NotImplementedException();
+            var entry = Db.Entry(obj); //aqui estamos recebendo os dados alterados pelo usuário
+            DbSet.Attach(obj); //aqui estamos recebendo os dados já inseridos no DB
+            entry.State = EntityState.Modified; //aqui iremos modificar somente os campos que foi alterado
+            SaveChanges(); //realizará a atualização
+             
+            return obj;
         }
     }
 }
