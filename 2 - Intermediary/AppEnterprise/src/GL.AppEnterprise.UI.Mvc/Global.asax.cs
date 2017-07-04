c22c60b9116a8e9f55a9125f6d4b90584cb96e59
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-07-03 22:54:01 -0300

    Incluindo no arquivo 'Global.asax' a config do 'AutoMapperConfig.cs'

--- 2 - Intermediary/AppEnterprise/src/GL.AppEnterprise.UI.Mvc/Global.asax.cs
+++ 2 - Intermediary/AppEnterprise/src/GL.AppEnterprise.UI.Mvc/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using GL.AppEnterprise.Application.AutoMapper;
 
 namespace GL.AppEnterprise.UI.Mvc
 {
@@ -16,6 +17,7 @@ namespace GL.AppEnterprise.UI.Mvc
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AutoMapperConfig.RegisterMappings();
         }
     }
 }
