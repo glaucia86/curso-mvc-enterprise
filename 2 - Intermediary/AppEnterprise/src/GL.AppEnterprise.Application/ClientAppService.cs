fdc3804040cc677d36701b0970ba3d73142e71c1
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-07-03 22:05:02 -0300

    Criação do arquivo 'ClientAppService.cs' na camada 'Application'

---
+++ 2 - Intermediary/AppEnterprise/src/GL.AppEnterprise.Application/ClientAppService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using GL.AppEnterprise.Application.Interfaces;
+using GL.AppEnterprise.Application.ViewModel;
+using GL.AppEnterprise.Infra.Data.Repositories;
+
+namespace GL.AppEnterprise.Application
+{
+    public class ClientAppService : IClientAppService
+    {
+        private readonly ClientRepository _clientRepository;
+
+        public ClientAppService()
+        {
+            _clientRepository = new ClientRepository();
+        }
+        
+        public ClientAddressViewModel Add(ClientAddressViewModel clientAddressViewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientViewModel GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientViewModel GetByCpf(string cpf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientViewModel GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ClientViewModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientViewModel Update(ClientViewModel clientViewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
